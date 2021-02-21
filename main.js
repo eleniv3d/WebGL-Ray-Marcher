654e09f67fb40f1bb3eb8d8bf6f104ad02eafe4e
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-02-21 22:28:31 +0100

    clean up

--- main.js
+++ main.js
@@ -253,15 +253,11 @@ var initCanvas = function () {
     var folder5 = gui.addFolder('shader');
     folder5.add(shader, 'type', [
         "gyroid",
-        "mandelbulb",
-        "spheres",
-        "clouded",
         "indexedGyroid",
+        "gyroidSmooth",
         "schwarzDPPGradient",
         "schwarzDPPIndexed",
-        "gyroidSmooth",
         "schwarzDPPSmooth",
-        "brickShader"
     ]).onChange( function () {
         switchShader(shader);
     });
@@ -341,14 +337,8 @@ var drawScene = function () {
 
 function switchShader() {
 
-    if (shader.type == "mandelbulb") {
-        frag = 'fragShader2'
-    } else if (shader.type == "gyroid") {
+    if (shader.type == "gyroid") {
         frag = 'fragShader'
-    } else if (shader.type == "spheres") {
-        frag = 'fragShader3'
-    } else if (shader.type == "clouded") {
-        frag = 'fragShader4'
     } else if (shader.type == "indexedGyroid") {
         frag = 'gyroidIndexed'
     } else if (shader.type == "schwarzDPPIndexed") {
@@ -359,9 +349,8 @@ function switchShader() {
         frag = 'schwarzDPPSmooth'
     } else if (shader.type == "gyroidSmooth") {
         frag = 'gyroidSmooth'
-    } else if (shader.type == "brickShader") {
-        frag = 'brickShader'
     }
+    
     shaderProgram = new Shader('vertShader', frag);
     // Activate the shader program
     shaderProgram.UseProgram();
